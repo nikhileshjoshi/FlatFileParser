5452845eac0cc42a4aaa8e73e78f3e7be838ae65
Nikhilesh Shankar <Nikhilesh_Shankar@vfc.com>
2017-03-31 17:11:12 -0400

    Work in progress

--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil")
+	"fmt"
+	"io/ioutil"
+  "strings"
+)
 
-type Pairs struct{
-  Id int
-  Name string
+type Pairs struct {
+	Id   int
+	Name string
 }
 
-func main()  {
-  bs,err := ioutil.ReadFile("test.gofig")
-  if err != nil{
-    panic(err)
-  }
+type configStruct struct{
+  column string
+  typeName string
+  loc string
+}
 
+func main() {
+	bs, err := ioutil.ReadFile("test.gofig")
+	if err != nil {
+		panic(err)
+	}
+  fmt.Println(string(bs))
+  GetStringBetween(string(bs),"{", "}")
+}
+
+func Decode(s string, &Interface{}, config configStruct){
+
+}
+
+func createConfigStruct(sArr []string){
+  for
+}
+
+func GetStringBetween(s string, prefix string, suffix string) {
+  fmt.Println(strings.Index(s, prefix), strings.Index(s, suffix))
+  i := strings.Index(s, prefix)
+  e := strings.Index(s, suffix)
+  fmt.Println(s[i+1:e])
+  arr := strings.Split(s[i+1:e], "\n")
+  fmt.Println("line count: " , len(arr))
+  var ar []string
+  for _, s := range arr{
+    if strings.TrimSpace(s) != ""{
+      ar = append(ar,s)
+    }
+  }
+  fmt.Println("line count: " , len(ar))
 }
